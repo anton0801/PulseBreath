21caab6c7a1375a8559f2da8a3a31f8a260cb2ea
Stepan <sfermin2007@icloud.com>
2025-11-06 15:21:32 +0100

    Initial Commit

---
+++ Pulse Breath/Pulse_BreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pulse_BreathApp.swift
+//  Pulse Breath
+//
+//  Created by Stepan Yarikova on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Pulse_BreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
